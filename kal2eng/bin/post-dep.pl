7fe5579f23cb0c494351f674e3b58a17fb1c4fda
Tino Didriksen <mail@tinodidriksen.com>
2020-10-01 09:09:55 +0000

    Forgot file
    git-svn-id: https://nutserut.gl@3021 665153f0-9ce2-47ca-83ac-daa507cf906a

---
+++ kal2eng/bin/post-dep.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/env perl
+# -*- mode: cperl; indent-tabs-mode: nil; tab-width: 3; cperl-indent-level: 3; -*-
+BEGIN { $| = 1; }
+use utf8;
+use warnings;
+use strict;
+
+while (<STDIN>) {
+   s~^\t(.+?)(<tr:[^>]+>.+?<tr-end>)~\t$2$1~g; # Swap inline translation+tags and current baseform+tags
+   s~<tr:([^>]+)>.+?<tr-end>~"$1"~g; # Turn inline translation into a baseform
+   print;
+}
