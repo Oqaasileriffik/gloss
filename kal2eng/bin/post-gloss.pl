1059d29ae062b7f1bc960707dcf76f970aec9b2b
Per Langgård <pela@oqaasileriffik.gl>
2020-10-05 16:42:10 +0000

    De allerførste regler
    git-svn-id: https://nutserut.gl@3059 665153f0-9ce2-47ca-83ac-daa507cf906a

--- kal2eng/bin/post-gloss.pl
+++ kal2eng/bin/post-gloss.pl
@@ -7,8 +7,15 @@ use strict;
 
 while (<STDIN>) {
    s~^\t(.+?) <tr-prefix> (<tr:[^>]+>.+?<tr-end>)~\t$2 $1~g; # Swap inline translation+tags and current baseform+tags
-   s~<tr:([^>]+)>.+?<tr-end>~"$1"~g; # Turn inline translation into a baseform
+  s~<tr:([^>]+)>.+?<tr-end>~"$1"~g; # Turn inline translation into a baseform
 
-   s~  +~ ~g; # Clean up spaces
+#Simple ledflytninger sent i kaskaden. Kun for de overordnede POS
+    s~^\t(.+?) (TUQ Der/vn N Lok Sg)~\t$2 $1~g; #Lok på verbalnomen
+    s~TUQ Der/vn N Lok Sg~"in/on what"~g; 
+
+    s~^\t(.+?) (V (Ind|Par) 3Sg)~\t$2 $1~g; #Subjekt og modus
+    s~V (Ind|Par) 3Sg~"he/she/it"~g; 
+    
+   s~ <tr>+~ ~g; # Clean up spaces
    print;
 }
