ff6c45d7267d06b7d7da9909e7f2abbb50cd6089
Per Langgård <pela@oqaasileriffik.gl>
2020-10-06 15:59:28 +0000

    git-svn-id: https://nutserut.gl@3080 665153f0-9ce2-47ca-83ac-daa507cf906a

--- kal2eng/bin/post-gloss.pl
+++ kal2eng/bin/post-gloss.pl
@@ -12,11 +12,17 @@ while (<STDIN>) {
 #Simple ledflytninger sent i kaskaden. Kun for de overordnede POS
    s~^\t(.+?) (TUQ Der/vn N Lok Sg)~\t$2 $1~g; #Lok på verbalnomen
    s~TUQ Der/vn N Lok Sg~"in/on what"~g; 
-
-   s~^\t(.+?) (V (Ind|Par) 3Sg)~\t$2 $1~g; #Subjekt og modus
+   
+   s~^\t(.+?) (Sem/Geo Prop Lok Sg)~\t$2 $1~g; #Lok på Geo/Prop
+   s~Sem/Geo Prop Lok Sg~"in"~g; 
+   
+   s~^\t(.+?) (V (Ind|Par) 3Sg)~\t$2 $1~g; #Subjekt Sg og modus
    s~V (Ind|Par) 3Sg~"he/she/it"~g; 
 
-   s~ <tr> ~ ~g; # Remove translation marker
+   s~^\t(.+?) (V (Ind|Par) 3Pl)~\t$2 $1~g; #Subjekt Pl og modus
+   s~V (Ind|Par) 3Pl~"they"~g; 
+
+#   s~ <tr> ~ ~g; # Remove translation marker
    s~  +~ ~g; # Clean up spaces
    print;
 }
